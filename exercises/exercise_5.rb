ff7003d6a55a5b4625314645445c5309b165304f
Edmund Yu <eyu1@ualberta.ca>
2021-11-09 19:37:15 +0000

    Implement exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,7 +4,10 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
-puts "Exercise 5"
-puts "----------"
+puts 'Exercise 5'
+puts '----------'
 
 # Your code goes here ...
+Store.sum(:annual_revenue)
+Store.average(:annual_revenue)
+Store.where('annual_revenue >= 1000000').count
